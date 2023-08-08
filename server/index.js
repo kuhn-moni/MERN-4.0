bb388be9947fea80359a8c5da9f5aaaf97704e72
kuhn-moni <kuhn.moonay@gmail.com>
2023-08-08 16:33:14 +0200

    gitignore updated, mongo uri added to env

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import userRouter from "./routes/user.js";
+import * as dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5000;
