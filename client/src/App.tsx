ac93a37527df77133816193e1aed016daf221cb5
kuhn-moni <kuhn.moonay@gmail.com>
2023-08-09 20:55:51 +0200

    user array displaying on console log

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Users } from "./@types";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  // console.log(import.meta.env.VITE_SERVER_BASE);
+  const [users, setUsers] = useState<Users>([]);
+  console.log(import.meta.env.VITE_SERVER_BASE);
+  useEffect(() => {
+    const fetchAllUsers = async () => {
+      try {
+        const response = await fetch(
+          `${import.meta.env.VITE_SERVER_BASE}api/users/all`
+        );
+        const result = await response.json();
+        setUsers(result);
+        console.log(result);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchAllUsers().catch((e) => console.log(e));
+  }, []);
 
   return (
     <>
@@ -29,7 +48,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
