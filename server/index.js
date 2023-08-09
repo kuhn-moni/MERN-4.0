19999c0074ac24a39de8e5c91f04aeca47f2a1a4
kuhn-moni <kuhn.moonay@gmail.com>
2023-08-09 13:52:48 +0200

    connected middlewares, database and routes defined

--- server/index.js
+++ server/index.js
@@ -1,30 +1,41 @@
 import express from "express";
+import mongoose from "mongoose";
 import cors from "cors";
 import userRouter from "./routes/user.js";
 import * as dotenv from "dotenv";
 dotenv.config();
 
+console.log("MOGO_URI", process.env.MONGO_URI);
+
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(express.json());
-app.use(
-  express.urlencoded({
-    extended: true,
-  })
-);
-app.use(cors());
+const connectMiddlewares = () => {
+  app.use(express.json());
+  app.use(
+    express.urlencoded({
+      extended: true,
+    })
+  );
+  app.use(cors());
+};
 
-app.listen(port, () => {
-  console.log("Server is running on port..." + port);
-});
+const connectDatabase = async () => {
+  await mongoose.connect(process.env.MONGO_URI);
+  app.listen(port, () => {
+    console.log("Server is running on port..." + port);
+  });
+};
+const defineRoutes = () => {
+  app.use("*", (req, res) =>
+    res.status(404).json({ error: "Endpoint not found." })
+  );
+  app.use("/api/users", userRouter);
+};
 
-app.use("*", (req, res) =>
-  res.status(404).json({ error: "Endpoint not found." })
-);
-
-app.use("/api/users", userRouter);
-
+connectMiddlewares();
+defineRoutes();
+connectDatabase();
 // app.get("/hello", (req, res) => {
 //   res.send("Hello World!");
 // });
