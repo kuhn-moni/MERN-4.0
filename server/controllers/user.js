b38180599afc7a86dbbde3a7e40973180dd86844
kuhn-moni <kuhn.moonay@gmail.com>
2023-08-10 21:45:24 +0200

    findAllUsers added

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -5,6 +5,20 @@ const testResponse = (req, res) => {
   res.send("We have a response!!!!!!");
 };
 
+const findAllUsers = async (req, res) => {
+  try {
+    const users = await UserModel.find();
+    if (users) {
+      res.status(200).json(users);
+    } else {
+      res.status(404).json({ error: "Nothing in the collection" });
+    }
+  } catch (e) {
+    res.status(500).json({ error: "Something went wrong?!" });
+  }
+};
+
+export { testResponse, findAllUsers };
 const findUserByEmail = async (req, res) => {
   const { email } = req.params;
   if (email && email.includes("@")) {
