f4b5199da0dfd7210981c6add8341c168929a921
kuhn-moni <kuhn.moonay@gmail.com>
2023-08-16 14:01:28 +0200

    sports_activites schema page created

--- server/models/user.js
+++ server/models/user.js
@@ -5,6 +5,7 @@ const userSchema = new mongoose.Schema(
     email: { type: String, required: true, unique: true },
     username: String,
     password: { type: String, required: true },
+    sports_activities: [],
   },
   { timestamps: true }
 );
