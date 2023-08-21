b54238e249479e98cbf093cfad4ef4b86ad905f5
kuhn-moni <kuhn.moonay@gmail.com>
2023-08-21 16:51:36 +0200

    profile pic upload, no placeholder present

--- client/src/components/UserCard.tsx
+++ client/src/components/UserCard.tsx
@@ -11,7 +11,12 @@ function UserCard({ user }: Props) {
         style={{ border: "solid 1px black", padding: "0 1em", width: "300px" }}
       >
         <p>
-          <b>{user.username}</b> - {user.email}
+          <b>{user.username}</b> - {user.email}{" "}
+          <img
+            src={user.profile_pic}
+            alt={`${user.username}'s profile picture`}
+            style={{ height: "50px", width: "50px" }}
+          />
         </p>
       </div>
     )
