35fd190887228717a96dc013139375c3a3bda5f1
kuhn-moni <kuhn.moonay@gmail.com>
2023-08-29 14:47:11 +0200

    AuthContext for Login, Login styling and useState

--- client/src/context/AuthContext.tsx
+++ client/src/context/AuthContext.tsx
@@ -1,9 +1,21 @@
-import { createContext } from "react";
+import { createContext, useState, ReactNode } from "react";
+import { NotOk, User } from "../@types";
+
+interface LoginResult {
+  verified: boolean;
+  token: string;
+}
 
 interface DefaultValue {
   user: null | User;
   login: (email: string, password: string) => Promise<void>;
   logout: () => void;
+}
+
+interface LoginResult {
+  verified: boolean;
+  token: string;
+  user: User;
 }
 
 const initialValue: DefaultValue = {
@@ -20,6 +32,47 @@ export const AuthContext = createContext<DefaultValue>(initialValue);
 
 export const AuthContextProvider = ({ children }: { children: ReactNode }) => {
   const [user, setUser] = useState(false);
+  const baseURL = import.meta.env.VITE_SERVER_BASE as string;
 
-  const login = email;
+  const login = (email: string, password: string) => {
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+    const urlencoded = new URLSearchParams();
+    urlencoded.append("email", email);
+    urlencoded.append("password", password);
+    const requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: urlencoded,
+    };
+    try {
+      const response = await fetch(`${baseURL}api/users/login`, requestOptions);
+      if (!response.ok) {
+        const result = (await response.json()) as NotOk;
+        alert(result.error);
+      } else {
+        const result = (await response.json()) as LoginResult;
+        console.log(result);
+        setUser(result.user);
+        localStorage.setItem("token", result.token);
+        localStorage.setItem("user", JSON.stringify(result.user));
+      }
+    } catch (error) {
+      console.log("error", error);
+      setEmail("");
+      setPassword("");
+    }
+    const logout = () => {
+      //logout logic goes here
+    };
+
+    //const getActiveUser = () => {
+    //logic goes here
+
+    return (
+      <AuthContext.Provider value={{ user, login, logout }}>
+        {children}
+      </AuthContext.Provider>
+    );
+  };
 };
