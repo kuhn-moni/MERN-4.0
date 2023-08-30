2d7ed5e44b9a73e1ad289a35e719f378bed74fb0
kuhn-moni <kuhn.moonay@gmail.com>
2023-08-30 11:11:17 +0200

    logout button, user login status display

--- client/src/components/Nav.tsx
+++ client/src/components/Nav.tsx
@@ -1,7 +1,10 @@
-// type Props = {}
+import { useContext } from "react";
 import { NavLink } from "react-router-dom";
+import { AuthContext } from "../context/AuthContext";
 
 function Nav() {
+  const { user, logout } = useContext(AuthContext);
+
   const navContainerStyles: React.CSSProperties = {
     width: "100%",
     height: "50px",
@@ -12,6 +15,11 @@ function Nav() {
     padding: "0 1em",
   };
 
+  const linksContainerStyles: React.CSSProperties = {
+    display: "flex",
+    gap: "1em",
+  };
+
   const activeLink: React.CSSProperties = {
     color: "red",
     fontWeight: "bold",
@@ -19,21 +27,18 @@ function Nav() {
 
   return (
     <nav style={navContainerStyles}>
-      <NavLink to="/" style={({ isActive }) => (isActive ? activeLink : {})}>
-        Homepage
-      </NavLink>
-      <NavLink
-        to="/users"
-        style={({ isActive }) => (isActive ? activeLink : {})}
-      >
-        Users
-      </NavLink>
-      <NavLink
-        to="/login"
-        style={({ isActive }) => (isActive ? activeLink : {})}
-      >
-        Login
-      </NavLink>
+      <div style={linksContainerStyles}>
+        <NavLink to="/" style={({ isActive }) => (isActive ? activeLink : {})}>
+          Homepage
+        </NavLink>
+        <NavLink to="/users" style={({ isActive }) => (isActive ? activeLink : {})}>
+          Users
+        </NavLink>
+        <NavLink to="/login" style={({ isActive }) => (isActive ? activeLink : {})}>
+          Login
+        </NavLink>
+      </div>
+      <p>{user ? <button onClick={logout}>Logout</button> : "User is logged out"}</p>
     </nav>
   );
 }
