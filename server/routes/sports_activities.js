651923a2ac8fbbcdeff17eafea9794de2ca3815d
kuhn-moni <kuhn.moonay@gmail.com>
2023-08-30 19:39:35 +0200

    getMe controller

--- server/routes/sports_activities.js
+++ server/routes/sports_activities.js
@@ -4,4 +4,7 @@ import { findAllActivities } from "../controllers/sports_activities.js";
 const activitiesRouter = express.Router();
 
 activitiesRouter.get("/all", findAllActivities);
+
+activitiesRouter.post("/new", createActivity);
+
 export default activitiesRouter;
