596737ff6d7a65ba445e29531f09e30a4b813cb0
kuhn-moni <kuhn.moonay@gmail.com>
2023-08-31 12:09:24 +0200

    post req for new activity working

--- server/controllers/sports_activities.js
+++ server/controllers/sports_activities.js
@@ -14,4 +14,25 @@ const findAllActivities = async (req, res) => {
   res.send("testing...");
 };
 
-export { findAllActivities };
+const createActivity = async (req, res) => {
+  console.log(req.body);
+  const testing = {
+    organiser: req.body.organiser,
+    participants: req.body.participants.split(","),
+    activity: req.body.activity,
+    duration: req.body.duration,
+    date: req.body.date,
+  };
+  console.log(testing);
+  try {
+    const newActivity = new activitiesModel(testing);
+    const createdActivity = await newActivity.save();
+
+    res.status(200).json(createdActivity);
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({ error: "Unknown error occured" });
+  }
+};
+
+export { findAllActivities, createActivity };
